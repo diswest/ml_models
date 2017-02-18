4957c798b50995a051330c30e07a9cd08419f25c
Ivan Miniailenko <discrete.west@gmail.com>
2017-02-19 02:23:06 +0300

    Improve gradient descent implementation

--- models/optimization.py
+++ models/optimization.py
@@ -6,15 +6,14 @@ def gd(X, y, gradient_f, cost_f=None, alpha=.01, n_iters=1000, tol=.001, debug=F
 
     coef = np.ones((X.shape[1], 1))
     costs = []
-    for _ in range(n_iters):
+    cost = cost_f(X, y, coef)
+    step = 0
+    while step < n_iters and cost > tol:
         gradient = gradient_f(X, y, coef)
         coef -= alpha * gradient
         cost = cost_f(X, y, coef)
+        step += 1
         if debug:
             costs.append(cost)
-
-        # check tolerance
-        if cost <= tol:
-            break
 
     return coef, costs
