9a90bb2f4e7ab948afd9d662a2b93da65e2ae068
Ivan Miniailenko <discrete.west@gmail.com>
2017-02-19 00:45:21 +0300

    Add RidgeRegression model

--- models/linear.py
+++ models/linear.py
@@ -75,3 +75,37 @@ class LinearRegression(BaseLinearModel):
         m = X.shape[0]
 
         return np.dot(X.T, loss) / m
+
+
+class RidgeRegression(LinearRegression):
+    """ Ridge regression (Linear regression with L2 regularization) """
+
+    def __init__(self, alpha=0.01, lambda_coef=0.1, n_iters=1000, tol=0.0001, debug=False):
+        self._lambda_coef = lambda_coef
+        super(RidgeRegression, self).__init__(
+            alpha=alpha,
+            n_iters=n_iters,
+            tol=tol,
+            debug=debug
+        )
+
+    def _loss(self, X, y, coef):
+        h = self._decision_function(X, coef)
+        return h - y
+
+    def _cost_f(self, X, y, coef):
+        loss = self._loss(X, y, coef)
+        m = X.shape[0]
+
+        # noinspection PyTypeChecker
+        penalty = self._lambda_coef * np.sum(coef ** 2)
+
+        return (np.sum(loss ** 2) + penalty) / (2 * m)
+
+    def _gradient_f(self, X, y, coef):
+        loss = self._loss(X, y, coef)
+        m = X.shape[0]
+
+        penalty = self._lambda_coef * np.sum(coef)
+
+        return (np.dot(X.T, loss) + penalty) / m
