0eebb1bfd10b4c6cb18262ec92406ef03113ebd9
Ivan Miniailenko <discrete.west@gmail.com>
2017-02-19 03:12:59 +0300

    Add dummies for optimization algorithms

--- models/optimization.py
+++ models/optimization.py
@@ -17,3 +17,54 @@ def gd(X, y, gradient_f, cost_f=None, alpha=.01, n_iters=1000, tol=.001, debug=F
             costs.append(cost)
 
     return coef, costs
+
+
+def cgd():
+    """ Conjugate gradient descent """
+    pass
+
+
+def mgd():
+    """ Mini-batch gradient descent """
+    pass
+
+
+def sgd():
+    """ Stochastic gradient descent """
+
+    # Momentum
+    # Averaging
+    # AdaGrad
+    # Adadelta
+    # RMSProp
+    # Adam
+    # Adamax
+    # Nadam
+    # kSGD
+
+    pass
+
+
+def pgd():
+    """ Proximal gradient descent """
+    pass
+
+
+def sag():
+    """ Stochastic average gradient """
+    pass
+
+
+def cd():
+    """ Coordinate descent """
+    pass
+
+
+def newton():
+    """ Newton's method """
+    pass
+
+
+def newton_cg():
+    """ Newton conjugate gradient """
+    pass
