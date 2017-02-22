e236ee7ba4bd7d34d80603356b32fc370259c9bd
Ivan Miniailenko <discrete.west@gmail.com>
2017-02-22 03:14:32 +0300

    Add coordinate descent, LASSO, fix some errors, refactoring code

--- models/linear.py
+++ models/linear.py
@@ -1,15 +1,15 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
-from .optimization import gd
+from .optimization import gd, cd
 
 
 class BaseLinearModel(metaclass=ABCMeta):
     """ Base linear model """
 
-    def __init__(self, alpha=.01, n_iters=1000, tol=.001, debug=False):
+    def __init__(self, n_iters=1000, tol=.0001, debug=False):
         self._coef = None
-        self._alpha = alpha
+        self._norm = None
         self._n_iters = n_iters
         self._debug = debug
         self._tol = tol
@@ -17,17 +17,10 @@ class BaseLinearModel(metaclass=ABCMeta):
     def fit(self, X, y):
         # add intercept column
         X_copy = np.insert(X, 0, [1], axis=1)
+        X_copy, self._norm = self._normalize(X_copy)
 
-        self._coef, cost = gd(
-            X_copy,
-            y,
-            gradient_f=self._gradient_f,
-            cost_f=self._cost_f,
-            alpha=self._alpha,
-            n_iters=self._n_iters,
-            tol=self._tol,
-            debug=self._debug
-        )
+        self._coef, cost = self._solver(X_copy, y)
+        self._coef = self._coef / self._norm.reshape(self._coef.shape)
 
         if self._debug:
             return cost
@@ -44,43 +37,63 @@ class BaseLinearModel(metaclass=ABCMeta):
     def _decision_function(X, coef):
         return np.dot(X, coef)
 
+    @staticmethod
+    def _normalize(X):
+        norm = np.sqrt(np.sum(X**2, axis=0))
+        return X / norm, norm
+
     @abstractmethod
     def _loss(self, X, y, coef):
         pass
 
     @abstractmethod
-    def _cost_f(self, X, y, coef):
-        pass
-
-    @abstractmethod
-    def _gradient_f(self, X, y, coef):
+    def _solver(self, X, y):
         pass
 
 
 class LinearRegression(BaseLinearModel):
     """ Linear regression """
 
+    def __init__(self, alpha=.01, n_iters=1000, tol=.0001, debug=False):
+        self._alpha = alpha
+        super(LinearRegression, self).__init__(
+            n_iters=n_iters,
+            tol=tol,
+            debug=debug
+        )
+
+    def _solver(self, X, y):
+        return gd(
+            X,
+            y,
+            gradient_f=self._gradient_f,
+            cost_f=self._cost_f,
+            alpha=self._alpha,
+            n_iters=self._n_iters,
+            tol=self._tol,
+            debug=self._debug
+        )
+
     def _loss(self, X, y, coef):
-        h = self._decision_function(X, coef)
-        return h - y
+        return y - self._decision_function(X, coef)
 
     def _cost_f(self, X, y, coef):
         loss = self._loss(X, y, coef)
         m = X.shape[0]
 
-        return (loss.T * loss) / (2 * m)
+        return np.sum(loss ** 2) / (2 * m)
 
     def _gradient_f(self, X, y, coef):
         loss = self._loss(X, y, coef)
         m = X.shape[0]
 
-        return np.dot(X.T, loss) / m
+        return -np.dot(X.T, loss) / m
 
 
-class RidgeRegression(BaseLinearModel):
+class RidgeRegression(LinearRegression):
     """ Ridge regression (Linear regression with L2 regularization) """
 
-    def __init__(self, alpha=.01, lambda_coef=.1, n_iters=1000, tol=.001, debug=False):
+    def __init__(self, alpha=.01, lambda_coef=.1, n_iters=1000, tol=.0001, debug=False):
         self._lambda_coef = lambda_coef
         super(RidgeRegression, self).__init__(
             alpha=alpha,
@@ -89,21 +102,60 @@ class RidgeRegression(BaseLinearModel):
             debug=debug
         )
 
-    def _loss(self, X, y, coef):
-        h = self._decision_function(X, coef)
-        return h - y
-
     def _cost_f(self, X, y, coef):
         loss = self._loss(X, y, coef)
         m = X.shape[0]
         penalty = self._lambda_coef * np.sum(np.dot(coef[1:].T, coef[1:]))
 
-        return (np.sum(np.dot(loss.T, loss)) + penalty) / (2 * m)
+        return (np.dot(loss.T, loss).flatten() + penalty) / (2 * m)
 
     def _gradient_f(self, X, y, coef):
         loss = self._loss(X, y, coef)
         m = X.shape[0]
         penalty = self._lambda_coef * np.sum(coef[1:])
 
-        # don't regularize intercept
-        return (np.dot(X.T, loss) + np.array([0, penalty]).reshape(2, 1)) / m
+        gradient = -np.dot(X.T, loss) + penalty
+        gradient[0] -= penalty
+
+        return gradient / m
+
+
+class Lasso(BaseLinearModel):
+    """ LASSO (Least Absolute Shrinkage and Selection Operator, linear regression with L1 regularization) """
+
+    def __init__(self, lambda_coef=.1, n_iters=1000, tol=.0001, debug=False):
+        self._lambda_coef = lambda_coef
+
+        super(Lasso, self).__init__(
+            n_iters=n_iters,
+            tol=tol,
+            debug=debug
+        )
+
+    def _solver(self, X, y):
+        return cd(
+            X,
+            y.flatten(),
+            optimize_f=self._optimize_f,
+            n_iters=self._n_iters,
+            tol=self._tol,
+            debug=self._debug
+        )
+
+    def _loss(self, X, y, coef):
+        return y - self._decision_function(X, coef)
+
+    def _optimize_f(self, X, y, coef, j):
+        loss = self._loss(X, y, coef)
+
+        # ro is greek ρ
+        ro = np.sum(np.dot(X[:, j].T, loss))
+        return ro if j == 0 else self._soft_threshold(ro)
+
+    def _soft_threshold(self, ro):
+        if ro < -self._lambda_coef/2:
+            return ro + self._lambda_coef/2
+        elif -self._lambda_coef/2 <= ro <= self._lambda_coef/2:
+            return 0
+        elif ro > self._lambda_coef/2:
+            return ro - self._lambda_coef/2
--- models/optimization.py
+++ models/optimization.py
@@ -1,7 +1,8 @@
 import numpy as np
+import sys
 
 
-def gd(X, y, gradient_f, cost_f=None, alpha=.01, n_iters=1000, tol=.001, debug=False):
+def gd(X, y, gradient_f, cost_f=None, alpha=.01, n_iters=1000, tol=.0001, debug=False):
     """ Full batch gradient descent """
 
     coef = np.ones((X.shape[1], 1))
@@ -33,7 +34,7 @@ def sgd():
     """ Stochastic gradient descent """
 
     # Momentum
-    # Averaging
+    # Averaging (sag?)
     # AdaGrad
     # Adadelta
     # RMSProp
@@ -55,9 +56,24 @@ def sag():
     pass
 
 
-def cd():
+def cd(X, y, optimize_f=None, n_iters=1000, tol=.001, debug=False):
     """ Coordinate descent """
-    pass
+    coef = np.zeros(X.shape[1])
+    costs = []
+    step = 0
+    cost = sys.float_info.max
+    while step < n_iters and cost > tol:
+        for j in range(len(coef)):
+            old_coef_j = coef[j].copy()
+            coef_copy = coef.copy()
+            coef_copy[j] = 0
+            coef[j] = optimize_f(X, y, coef_copy, j)
+            cost = abs(coef[j] - old_coef_j)
+            if debug:
+                costs.append(cost)
+        step += 1
+
+    return coef, costs
 
 
 def newton():
